ad5b8b0605401551218539a73ade7026530d831c
RuslanSharapov <113802749+RuslanSharapov@users.noreply.github.com>
2023-02-14 19:01:56 +0300

    Список неповторяющихся элементов

---
+++ work3.py
@@ -0,0 +1,16 @@
+#   Задайте последовательность чисел. Напишите программу, 
+#   которая выведет список неповторяющихся элементов исходной последовательности.
+
+import random
+
+n = int(input('Введите число: '))
+my_list = []
+new_list = []
+for i in range(n):
+    my_list.append(random.randint(1, 9))
+print(my_list)
+for i in my_list:
+    if i not in new_list:
+        new_list.append(i)
+print(new_list)
+
