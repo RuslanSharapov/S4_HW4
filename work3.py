f024f6483f252e46b4b8c00542a0618ad595122e
RuslanSharapov <113802749+RuslanSharapov@users.noreply.github.com>
2023-02-14 19:18:20 +0300

    Вывести список неповторяющихся элементов исходной последовательности

--- work3.py
+++ work3.py
@@ -14,3 +14,15 @@ for i in my_list:
         new_list.append(i)
 print(new_list)
 
+# С применением встроенной функции set
+
+""" import random
+
+n = int(input('Введите число: '))
+my_list = []
+
+for i in range(n):
+    my_list.append(random.randint(1, 9))
+print(my_list)
+print(list(set(my_list))) """
+
